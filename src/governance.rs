4b30291b54928557b909770cbe1b1139072d126b
slabgames <55172860+slabgames@users.noreply.github.com>
2025-03-13 23:08:48 +0800

    commit challenge

--- src/governance.rs
+++ src/governance.rs
@@ -26,16 +26,35 @@ pub struct GovernancePallet<T: GovernanceConfig> {
 
 impl<T: GovernanceConfig> GovernancePallet<T> {
     pub fn new() -> Self {
-        todo!()
+        // todo!()
+        // Initialize the GovernancePallet
+        Self {
+            proposals: HashMap::new(),
+            votes: HashMap::new(),
+            next_proposal_id: 0,
+        }
     }
 
     // Create a new proposal
     pub fn create_proposal(
         &mut self,
-        creator: T::AccountId,
+        _creator: T::AccountId,
         description: String,
     ) -> Result<u32, &'static str> {
-        todo!()
+        // todo!()
+        // create a new proposal and insert it into the proposals hashmap with the next_proposal_id as the key
+        let proposal_id = self.next_proposal_id;
+        let proposal = Proposal {
+            description,
+            yes_votes: 0,
+            no_votes: 0,
+            status: ProposalStatus::Active,
+        };
+
+        self.proposals.insert(proposal_id, proposal);
+        self.next_proposal_id += 1;
+
+        Ok(proposal_id)
     }
 
     // Vote on a proposal (true = yes, false = no)
@@ -45,17 +64,56 @@ impl<T: GovernanceConfig> GovernancePallet<T> {
         proposal_id: u32,
         vote_type: bool,
     ) -> Result<(), &'static str> {
-        todo!()
+        // todo!()
+                // check if the proposal exists in the proposals hashmap using the proposal_id
+        // if the voter has already voted, return an error
+        // if the voter has not voted, increment the yes_votes or no_votes count based on the vote_type
+        // insert the vote into the votes hashmap
+        if let Some(proposal) = self.proposals.get_mut(&proposal_id) {
+            if let Some(vote) = self.votes.get(&(voter.clone(), proposal_id)) {
+                if *vote == vote_type {
+                    return Err("You have already voted with the same vote type");
+                }
+            }
+
+            if vote_type {
+                proposal.yes_votes += 1;
+            } else {
+                proposal.no_votes += 1;
+            }
+
+            self.votes.insert((voter.clone(), proposal_id), vote_type);
+            Ok(())
+        } else {
+            Err("Proposal does not exist")
+        }
     }
 
     // Get proposal details
     pub fn get_proposal(&self, proposal_id: u32) -> Option<&Proposal> {
-        todo!()
+        // todo!()
+        // Return the proposal details if it exists
+        self.proposals.get(&proposal_id)
     }
 
     // Finalize a proposal (changes status based on votes)
     pub fn finalize_proposal(&mut self, proposal_id: u32) -> Result<ProposalStatus, &'static str> {
-        todo!()
+        // todo!()
+        // Check if the proposal exists
+        if !self.proposals.contains_key(&proposal_id) {
+            return Err("Proposal does not exist");
+        }
+
+        // Get the proposal
+        let proposal = self.proposals.get_mut(&proposal_id).unwrap();
+        // Update the proposal's status based on votes
+        if proposal.yes_votes > proposal.no_votes {
+            proposal.status = ProposalStatus::Approved;
+        } else {
+            proposal.status = ProposalStatus::Rejected;
+        }
+        // Return the proposal status
+        Ok(proposal.status.clone())
     }
 }
 
--- src/staking.rs
+++ src/staking.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 
 pub trait StakingConfig: SystemConfig {
     // Define the Balance type with ability to perform checked arithmetic operations
-    type Balance: Zero + CheckedSub + CheckedAdd + Copy;
+    type Balance: Zero + CheckedSub + CheckedAdd + Copy + PartialOrd;
 }
 
 pub struct StakingPallet<T: StakingConfig> {
@@ -16,32 +16,78 @@ pub struct StakingPallet<T: StakingConfig> {
 
 impl<T: StakingConfig> StakingPallet<T> {
     pub fn new() -> Self {
-        todo!()
+        // todo!()
+        // Initialize the StakingPallet
+        Self {
+            free_balances: HashMap::new(),
+            staked_balances: HashMap::new(),
+        }
     }
 
     // Set free balance for an account
     pub fn set_balance(&mut self, who: T::AccountId, amount: T::Balance) {
-        todo!()
+        // todo!()
+        // Check if the amount is zero
+        if amount.is_zero() {
+            // Remove the balance from the free_balances map
+            self.free_balances.remove(&who);
+            return;
+        }
+        // Insert the balance into the free_balances map
+        self.free_balances.insert(who, amount);
     }
 
     // Stake tokens (move from free to staked)
     pub fn stake(&mut self, who: T::AccountId, amount: T::Balance) -> Result<(), &'static str> {
-        todo!()
+        // todo!()
+        // check if the account has enough free balance to stake the amount
+        // if so, move the amount from free to staked balances
+        let zero_balance = T::Balance::zero();
+        let free_balance = self.free_balances.get(&who).unwrap_or(&zero_balance);
+        if amount > *free_balance {
+            return Err("Insufficient balance");
+        }
+        else {
+            let staked_balance = self.staked_balances.entry(who.clone()).or_insert(T::Balance::zero());
+            *staked_balance = staked_balance.checked_add(&amount).unwrap();
+            let new_free_balance = free_balance.checked_sub(&amount).unwrap();
+            self.free_balances.insert(who, new_free_balance);
+            Ok(())
+        }
     }
 
     // Unstake tokens (move from staked to free)
     pub fn unstake(&mut self, who: T::AccountId, amount: T::Balance) -> Result<(), &'static str> {
-        todo!()
+        // todo!()
+        // check if the account has enough staked balance to unstake the amount
+        // if so, move the amount from staked to free balances
+        let zero_balance = T::Balance::zero();
+        let staked_balance = self.staked_balances.get(&who).unwrap_or(&zero_balance);
+        if amount > *staked_balance {
+            return Err("Insufficient staked balance");
+        }
+        else {
+
+            let free_balance = self.free_balances.entry(who.clone()).or_insert(T::Balance::zero());
+            *free_balance = free_balance.checked_add(&amount).unwrap();
+            let new_staked_balance = staked_balance.checked_sub(&amount).unwrap();
+            self.staked_balances.insert(who, new_staked_balance);
+            Ok(())
+        }
     }
 
     // Get free balance for an account
     pub fn get_free_balance(&self, who: T::AccountId) -> T::Balance {
-        todo!()
+        // todo!()
+        // Return the free balance if it exists
+        self.free_balances.get(&who).copied().unwrap_or_else(T::Balance::zero)
     }
 
     // Get staked balance for an account
     pub fn get_staked_balance(&self, who: T::AccountId) -> T::Balance {
-        todo!()
+        // todo!()
+        // Return the staked balance if it exists
+        self.staked_balances.get(&who).copied().unwrap_or_else(T::Balance::zero)
     }
 }
 
